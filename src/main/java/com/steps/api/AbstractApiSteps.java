9314b8c5133edeb2163d2f58c66c1bdc6c866ef1
horialex <encianhoratiu@gmail.com>
2018-06-19 13:22:26 +0300

    Update AbstractApiSteps.java
    changed request spec to multipart

--- src/main/java/com/steps/api/AbstractApiSteps.java
+++ src/main/java/com/steps/api/AbstractApiSteps.java
@@ -95,7 +95,7 @@ public class AbstractApiSteps {
 	
 	protected String uploadCSVResource (String path, String fileName) {
 		 return given().relaxedHTTPSValidation()
-			.spec(getCsvMultipartSpec())
+			.spec(getMultipartSpec())
 			.multiPart(new MultiPartSpecBuilder(new File(System.getProperty("user.dir") + EnvironmentConstants.CSV_RESOURCES_PATH + fileName)).fileName(fileName).mimeType("application/vnd.ms-excel").build())
 			.when().post(path)
 			.then()
@@ -105,7 +105,7 @@ public class AbstractApiSteps {
 	
 	protected String createItemFromCSV(String path, String fileName, Category category) {
 		 return given().relaxedHTTPSValidation()
-					.spec(getCsvMultipartSpec())
+					.spec(getMultipartSpec())
 					.when().post(path + "?filename=" + fileName + "&" + "category_id="+category.getId())
 					.then()
 					.assertThat().statusCode(anyOf(is(201),is(204), is(200), is(302)))
